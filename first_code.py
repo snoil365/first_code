8e7f637bff8e7b90a0ab9308f9ec3813f6b0ccb0
snoil365 <snoil365@naver.com>
2023-10-04 15:10:45 +0900

    나의 첫 번째 파이썬 프로그램

---
+++ first_code.py
@@ -0,0 +1,8 @@
+print("hello world") 
+print("This is the first code") 
+total = 0  # 합계를 저장할 변수를 초기화합니다.
+
+for i in range(1, 11):  # 1부터 10까지의 숫자를 반복합니다.
+    total += i  # 현재 숫자를 합계에 더합니다.
+
+print("1부터 10까지의 합은:", total)  # 결과를 출력합니다. 
